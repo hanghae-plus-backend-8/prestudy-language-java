77886c9e6592cde9f9c031aaa47a54ce1742958f
discphy <dinophy@nate.com>
2025-02-14 08:25:52 +0900

    :sparkles: 자바 3주차 숙제 - 계산기 만들기 STEP 1

---
+++ playground/src/main/java/study/week2/calculator/Calculator.java
@@ -0,0 +1,14 @@
+package study.week2.calculator;
+
+public class Calculator {
+
+    public double calculate(String operator, int firstNumber, int secondNumber) {
+        return switch (operator) {
+            case "+" -> firstNumber + secondNumber;
+            case "-" -> firstNumber - secondNumber;
+            case "*" -> firstNumber * secondNumber;
+            case "/" -> (double) firstNumber / secondNumber;
+            default -> 0;
+        };
+    }
+}
